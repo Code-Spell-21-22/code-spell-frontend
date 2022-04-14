bf16aafbae24761a592cc0b4e14b1aa09b20e7b4
Danielar0w0 <danielar0000@gmail.com>
2022-04-14 15:29:24 +0100

    Implemented Login and Sign Up pages

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './index.css';
 import {SmallPanel} from "./components/SmallPanel/SmallPanel";
 import {faPlug} from "@fortawesome/free-solid-svg-icons";
-import {Container} from "react-bootstrap";
+import {Container, Image} from "react-bootstrap";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 
@@ -13,29 +13,49 @@ import {
 
 import {Dashboard} from "./components/Dashboard/Dashboard";
 import {Navbar} from "./components/Navbar/Navbar";
+import {Login} from "./components/Login/Login";
+import {SignUp} from "./components/SignUp/SignUp";
+
+let logged_in = false;
 import {Levels} from "./components/Levels/Levels";
 
 function App() {
-  return (
-      <div style={{backgroundImage: "url(/Background_2.png)",
-          backgroundRepeat:"no-repeat", backgroundSize:"cover", height: "100vh", width: "100vw"}}>
-          <Container>
-              <Row className="mx-5">
-                  <Navbar />
-              </Row>
-              <Row className="mx-5">
-                  <Router>
-                      <Routes>
-                          <Route  path="/" element={<Dashboard />}/>
-                          <Route  path="/levels" element={<Levels />}/>
-                      </Routes>
-                  </Router>
-              </Row>
-          </Container>
 
-      </div>
+    if (!logged_in) {
+        return (<div style={{backgroundImage: "url(/Background_2.png)",
+            backgroundRepeat:"no-repeat", backgroundSize:"cover", height: "100vh", width: "100vw"}}>
+            <Container className="justify-content-center d-flex">
+                <Router>
+                    <Routes>
+                        <Route path="/" element={<SignUp />}/>
+                        <Route path="/login" element={<Login />}/>
+                        <Route path="*" element={<SignUp />}/>
+                    </Routes>
+                </Router>
+            </Container>
+        </div>
+        );
+    }
 
-  );
+
+    return (
+        <div style={{backgroundImage: "url(/Background_2.png)",
+            backgroundRepeat:"no-repeat", backgroundSize:"cover", height: "100vh", width: "100vw"}}>
+            <Container>
+                <Row className="mx-5">
+                    <Navbar />
+                </Row>
+                <Row className="mx-5">
+                    <Router>
+                        <Routes>
+                            <Route path="/" element={<Dashboard />}/>
+                            <Route  path="/levels" element={<Levels />}/>
+                        </Routes>
+                    </Router>
+                </Row>
+              </Container>
+          </div>
+    );
 }
 
 export default App;
