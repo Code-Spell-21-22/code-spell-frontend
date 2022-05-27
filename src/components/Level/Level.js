43c664fbaa19958e0d7b1579981553c99b074872
DXOGO <diogophc@ua.pt>
2022-05-27 19:42:50 +0100

    level2_3 in the works

--- src/components/Level/Level.js
+++ src/components/Level/Level.js
@@ -117,7 +117,7 @@ export class Level extends React.Component {
                     <Col className="p-3 mb-4 col-4" style={{height: "78vh", borderRadius: "10px"}} >
 
                         {/* ! nome deste component tem de mudar conforme nivel (Level1_1, Level2_1, etc) */}
-                        <Level1_1 className="m-0" />                            
+                        <Level2_3 className="m-0" />                            
                     </Col>
 
 
