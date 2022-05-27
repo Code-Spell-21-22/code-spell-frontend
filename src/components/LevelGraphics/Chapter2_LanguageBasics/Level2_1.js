e9462fe1f5ac2a71bce1e03320ee68f70cd443ce
DXOGO <diogophc@ua.pt>
2022-05-27 19:03:04 +0100

    level 2_1 done

--- src/components/LevelGraphics/Chapter2_LanguageBasics/Level2_1.js
+++ src/components/LevelGraphics/Chapter2_LanguageBasics/Level2_1.js
@@ -2,39 +2,76 @@ import React, { useEffect, useRef } from "react";
 
 import * as THREE from "three";
 
+import {createInventory, createSword, createShield} from '../Builders/createItems';
 import {createScene, createCamera} from '../Builders/createEnvironment';
+import {createMovement} from '../Builders/tweenMotions';
 import {createPlayer} from '../Builders/createPlayer';
 import {createText} from '../Builders/createText';
+import {ageEditor} from '../Builders/ageEditor'
+
+const TWEEN = require('@tweenjs/tween.js')
 
 let camera, scene, renderer;
-var step1 = true; var step2 = true;
 
-// * Hello World
-const Level1_1 = () => {
- 
+var step1 = true; var step2 = true; var step3 = false;
+
+// * Variables
+const Level2_1 = () => {
+    
     useEffect(() => {
         // create camera and scene
+        
+
         // this is default camera 
         //createCamera = (posx, posy, posz, lx, ly, lz) - pos (camera position), - l (camera lookAt)
-        camera = createCamera(0, 7, 24, 0, 0, 0);
+        camera = createCamera(0, 6.5, 34, 0, 4, 0);
         scene = createScene();
         
-        // * create player => THIS IS GOING TO BE TRIGGERED BY USER CODE - STEP 1
-        if (step1 === true) {
-            const player = createPlayer();
-            scene.add(player); 
+        // player
+        const player = createPlayer();
+        player.position.set(-8 ,2, 8)
+        scene.add(player); 
+
+        // inventory
+        createInventory(scene);
+
+        // sword
+        const sword = createSword();
+        scene.add(sword);
+
+        // shield
+        const shield = createShield();
+        scene.add(shield);
+
+
+        // ! this response is sent from backend
+        // [player object, age]
+        var step1_response = [player, 30];
+
+        // * edit player age => THIS IS GOING TO BE TRIGGERED BY USER CODE -STEP 1
+        if (step1 === true){
+            const ageBox = ageEditor(step1_response);
+            scene.add(ageBox);
         }
-    
-        // ! this response comes from backend
-        const step2_response = "I'm trying different words, whats up?!";
+
+        // ! this response is sent from backend
+        var step2_response = "I'm learning variables";
         
         // * create text => THIS IS GOING TO BE TRIGGERED BY USER CODE -STEP 2
-        // const createText = (text, fontSize, textColor, hasSpeechBubble, hasTri, bubbleColor)
-        if (step1 == true && step2 === true){
+        // createText = (text, fontSize, textColor, hasSpeechBubble, hasTri, bubbleColor)
+        if (step2 === true){
             const text =  createText(step2_response, 0.5, 0x171717, true, true, 0xffffff);
+            text.position.set(player.position.x, player.position.y-2, player.position.z)
             scene.add(text)
-        } 
-        
+        }
+
+        // * create movement => THIS IS GOING TO BE TRIGGERED BY USER CODE -STEP 3
+        //  const createMovement = (obj, x, y, z, timeTo, delay)
+        if (step3 === true){
+            createMovement(sword, 1, -5.8, 0, 800, '+2000');
+            createMovement(shield, -0.5, -5, 0, 800, '+2000'); 
+        }
+
         /////////////////////////////////////////////////////////////
         renderer = new THREE.WebGLRenderer( { antialias: true } );
         renderer.setPixelRatio( window.devicePixelRatio );
@@ -53,8 +90,9 @@ const Level1_1 = () => {
 
         window.addEventListener( 'resize', onWindowResize );    
 
-        var animate = function() { 
+        var animate = function(time) { 
             requestAnimationFrame(animate);
+            TWEEN.update(time)
             renderer.render( scene, camera ); 
         };
 
@@ -68,4 +106,4 @@ const Level1_1 = () => {
 
  };
   
- export default Level1_1;
+ export default Level2_1;
