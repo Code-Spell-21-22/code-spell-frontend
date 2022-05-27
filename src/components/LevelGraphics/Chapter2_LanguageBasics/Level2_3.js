5a9292875fa3c177b646ddb9441e1199c40808fe
DXOGO <diogophc@ua.pt>
2022-05-28 00:52:11 +0100

    lab2_3 done

--- src/components/LevelGraphics/Chapter2_LanguageBasics/Level2_3.js
+++ src/components/LevelGraphics/Chapter2_LanguageBasics/Level2_3.js
@@ -2,13 +2,16 @@ import React, { useEffect, useRef } from "react";
 
 import * as THREE from "three";
 
+import {resizeMovement, createMovement, transitionColor} from '../Builders/tweenMotions';
 import {createScene, createCamera} from '../Builders/createEnvironment';
 import {createDay, createNight} from '../Builders/createSky';
 import {createPlayer} from '../Builders/createPlayer';
-import {createText} from '../Builders/createText';
+import {createText, showText} from '../Builders/createText';
+
+const TWEEN = require('@tweenjs/tween.js')
 
 let camera, scene, renderer;
-var step1 = true; var step2 = true; var step3 = true;
+var step1 = true; var step2 = false; var step3 = false;
 
 // * The if-then and if-then-else Statements
 const Level2_3 = () => {
@@ -23,22 +26,66 @@ const Level2_3 = () => {
         const player = createPlayer();
         player.position.set(0 ,2, 12)
         scene.add(player); 
+        
+        // by default is day
+        scene.background = new THREE.Color(0x2f78e1)
+        const day = createDay()
+        scene.add(day)
 
-        /* 
-        boolean day = true;
-        int stars;
+        var stars = 60;
+        
+        //* STEP1 - Update the value of variable step1_response to false. (is true by default)
+        // ! this response comes from backend
+        var step1_response = false;  // should be false so it becomes night
+        
+        if (step1 == true){
+            
+            if (step1_response === false){   // then its night
 
-        if (day) {
-            stars = 0;
-            dayTime();
-        } else {
-            stars = 10;
-            nightTime();  
+                if (step2 === false ) { // lets make some transitions
+                    createMovement(day, 50, 0, 0, 2000, '+2000')
+    
+                    const night = createNight(stars)
+                    night.position.set(-300, 0, 0);
+                    transitionColor(scene, 0x142433, 1000, '+0')
+                    createMovement(night, 0, 0, 0, 2000, '+0')
+                    scene.add(night)
+                }
+                
+                if (step2 === true) {   // no transitions
+                    scene.background = new THREE.Color(0x142433);
+                    scene.remove(day)
+                    scene.add(createNight(stars))
+                }
+            }
         }
 
-        */
+        //* STEP2 - Write an if-then-else statement that evaluates if variable stars has a value greater than 40
+        // ! this response comes from backend
+        var step2_response = [40, "What a starry sky!","What a beautiful sky!"] 
+        
+        if (step2 == true){
 
+            var step2_1 = step2_response[0]; var step2_2 = step2_response[1]; var step2_3 = step2_response[2];
+            
+            if (stars > step2_1) { showText(createText(step2_2, 0.5, 0x171717, true, true, 0xffffff), scene, player) } 
+            else { showText(createText(step2_3, 0.5, 0x171717, true, true, 0xffffff), scene, player) }
+        }
 
+        //* STEP3 -Rewrite the previous if-then-else statement in order to only print "So many stars!" to standard output if the value of stars is equal to 50. 
+        // ! this response comes from backend
+        var step3_response = [50, "So many stars!", 40, "What a starry sky!","What a beautiful sky!"] 
+        
+        if (step3 === true) {
+
+            var step3_1 = step3_response[0]; var step3_2 = step3_response[1]; var step3_3 = step3_response[2];
+            var step3_4 = step3_response[3]; var step3_5 = step3_response[4];
+
+            if (stars === step3_1) { showText(createText(step3_2, 0.5, 0x171717, true, true, 0xffffff), scene, player) } 
+            else if (stars > step3_3) { showText(createText(step3_4, 0.5, 0x171717, true, true, 0xffffff), scene, player) } 
+            else { showText(createText(step3_5, 0.5, 0x171717, true, true, 0xffffff), scene, player) }
+
+        }
 
         /////////////////////////////////////////////////////////////
         renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -58,7 +105,8 @@ const Level2_3 = () => {
 
         window.addEventListener( 'resize', onWindowResize );    
 
-        var animate = function() { 
+        var animate = function(time) {
+            TWEEN.update(time) 
             requestAnimationFrame(animate);
             renderer.render( scene, camera ); 
         };
