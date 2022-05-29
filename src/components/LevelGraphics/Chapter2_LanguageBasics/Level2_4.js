41e3d22a4000e2d058e3fc1a15487b18c57aa23c
DXOGO <diogophc@ua.pt>
2022-05-29 15:35:24 +0100

    level2_4 done

--- src/components/LevelGraphics/Chapter2_LanguageBasics/Level2_4.js
+++ src/components/LevelGraphics/Chapter2_LanguageBasics/Level2_4.js
@@ -2,15 +2,15 @@ import React, { useEffect, useRef } from "react";
 
 import * as THREE from "three";
 
-import {createMovement, transitionColor} from '../Builders/tweenMotions';
 import {createScene, createCamera} from '../Builders/createEnvironment';
-import {createPlayer} from '../Builders/createPlayer';
 import {createText, showText} from '../Builders/createText';
+import {createPlayer} from '../Builders/createPlayer';
+import {createNight} from '../Builders/createSky';
 
 const TWEEN = require('@tweenjs/tween.js')
 
 let camera, scene, renderer;
-var step1 = true; var step2 = true; var step3 = true;
+var step1 = true; var step2 = false;
 
 // * The switch Statement
 const Level2_4 = () => {
@@ -25,6 +25,41 @@ const Level2_4 = () => {
         const player = createPlayer();
         player.position.set(0 ,2, 12)
         scene.add(player); 
+
+        var stars = 50;
+        scene.background = new THREE.Color(0x142433);
+        scene.add(createNight(stars))
+
+
+        //* STEP1 -Rewrite the if-then-else statement from the previous level into a switch statement
+        // ! this response comes from backend
+        var step1_response = [50, "So many stars!", 40, "What a starry sky!","What a beautiful sky!"] 
+        
+        //* STEP2 - Change the value of the variable stars to study the behavior of the switch statement
+        // ! this response comes from backend
+        var step2_response = 80;    // player can change stars value 
+        
+        if (step1 === true) {
+
+            if (step2 === true) {
+                stars = step2_response
+            }
+
+            var step1_1 = step1_response[0]; var step1_2 = step1_response[1]; var step1_3 = step1_response[2];
+            var step1_4 = step1_response[3]; var step1_5 = step1_response[4];
+
+            if (stars === step1_1) { 
+                showText(createText(step1_2, 0.5, 0x171717, true, true, 0xffffff), scene, player) 
+            } 
+            else if (stars > step1_3) { 
+                showText(createText(step1_4, 0.5, 0x171717, true, true, 0xffffff), scene, player) 
+            } 
+            else { 
+                showText(createText(step1_5, 0.5, 0x171717, true, true, 0xffffff), scene, player) 
+            }
+
+        }
+
 
         /////////////////////////////////////////////////////////////
         renderer = new THREE.WebGLRenderer( { antialias: true } );
@@ -45,7 +80,7 @@ const Level2_4 = () => {
         window.addEventListener( 'resize', onWindowResize );    
 
         var animate = function(time) {
-            TWEEN.update(time) 
+            TWEEN.update(time);
             requestAnimationFrame(animate);
             renderer.render( scene, camera ); 
         };
