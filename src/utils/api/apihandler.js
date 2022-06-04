7662f220f4a60642e86bce8c162d4163f3fb78ce
Hugo1307 <hugomiguel_2001_@sapo.pt>
2022-06-04 19:53:50 +0100

    Refactor: Added support to environment variables

--- src/utils/api/apihandler.js
+++ src/utils/api/apihandler.js
@@ -1,130 +1,116 @@
 import axios from "axios";
 
+const apiAddress = process.env.REACT_APP_API_URL + "/api";
 
- const address ='http://159.65.60.64:8080/api';
-    
-async function postLogin  (email,password)  {
-        return axios.post(address+'/auth/login', {
-            email: email,
-            password: password
-        });
-    };
-    
-    async function postRegister  (username,email,password)  {
-        return axios.post(address+'/auth/register', {
-            username:username,
-            email: email,
-            password: password
-        });
-    };
+async function postLogin(email, password) {
+    return axios.post(apiAddress + '/auth/login', {
+        email: email,
+        password: password
+    });
+}
 
-     async function  getUserDetails  (username)  {
-        return axios.get(address+'/user/'+username+'/details');
-    };
-    
-    
-    /** 
-     * 
-     * params:{ username:username} 
-     *
-     * ns como meter no meio  
-     * 
-     */
+async function postRegister(username, email, password) {
+    return axios.post(apiAddress + '/auth/register', {
+        username: username,
+        email: email,
+        password: password
+    });
+}
 
+async function getUserDetails(username) {
+    return axios.get(apiAddress + '/user/' + username + '/details');
+}
 
-      async function  getUserAchievements (username) {
-        return axios.get(address+ '/user/'+username+'/achievements');
+async function getUserAchievements(username) {
+    return axios.get(apiAddress + '/user/' + username + '/achievements');
+}
 
-    };
-     async function  putUserPassword  (username,password) {
-        return axios.put(address+'/user/'+username+'/password',{
-            new_password: password
-        });
+async function putUserPassword(username, password) {
+    return axios.put(apiAddress + '/user/' + username + '/password', {
+        new_password: password
+    });
+}
 
-    };
-     async function   putUserName  (username,name) {
-        return axios.put(address+'/user/'+username+'/name',{
-            new_name:name
-
-        });
+async function putUserName(username, name) {
+    return axios.put(apiAddress + '/user/' + username + '/name', {
+        new_name: name
+    });
+}
 
 
-    };
+async function getLevelLeaderboard(levelid, language, level, skill_level) {
+    return axios.get(apiAddress + '/level/' + levelid + '/leaderboards', {
+        params: {
+            language: language,
+            level: level,
+            skill_level: skill_level
+        }
+    });
+}
 
-    
-     async function  getLevelLeaderboard    (levelid,language,level,skill_level) {
-        return axios.get(address+'/level/'+levelid+'/leaderboards',{
-            params: {
-                language:language,
-                level:level,
-                skill_level:skill_level
-            }
-        });
-    };
-     async function  getLevelDocumentation   (levelid) {
-        return axios.get(address+'/level/'+levelid+'/documentation')
-    };
-    
-     async function  getLevels    (language,skill_level,chapter) {
-        return axios.get(address+'/level',{
-            params: {
-                language:language,
-                skill_level:skill_level,
-                chapter:chapter
-            }
-        });
-    };
-     async function  getChapter    (language,skill_level) {
-        return axios.get(address+'/chapter',{
-            params: {
-                language:language,
-                skill_level:skill_level,
-            }
-        });
-    };
-     async function  getPossibleSoluctions   (levelid) {
-        return axios.get(address+'/level/'+levelid+'/solutions')
-    };
+async function getLevelDocumentation(levelid) {
+    return axios.get(apiAddress + '/level/' + levelid + '/documentation')
+}
+
+async function getLevels(language, skill_level, chapter) {
+    return axios.get(apiAddress + '/level', {
+        params: {
+            language: language,
+            skill_level: skill_level,
+            chapter: chapter
+        }
+    });
+}
+
+async function getChapter(language, skill_level) {
+    return axios.get(apiAddress + '/chapter', {
+        params: {
+            language: language,
+            skill_level: skill_level,
+        }
+    });
+}
+
+async function getPossibleSoluctions(levelid) {
+    return axios.get(apiAddress + '/level/' + levelid + '/solutions')
+}
 
 
-     async function  postLevelSolution   (levelid,solutionid,code,header)  {
-        return axios.post(address+'/level/'+levelid+'/submit/'+solutionid,code ,{
-            'authorization': header,
-            'content-type': 'text/plain',
-            
-        })   ;
-        };
-    
+async function postLevelSolution(levelid, solutionid, code, header) {
+    return axios.post(apiAddress + '/level/' + levelid + '/submit/' + solutionid, code, {
+        'authorization': header,
+        'content-type': 'text/plain',
+    });
+}
 
-         async function  getLevelGoals   (levelid) {
-        return axios.get(address+'/level/'+levelid+'/goals');
-    };
 
-     async function  getLevel   (levelid) {
+async function getLevelGoals(levelid) {
+    return axios.get(apiAddress + '/level/' + levelid + '/goals');
+}
 
-        return axios.get(address+'/level/'+levelid);
-    };
-     async function  getSolution   (levelid,solutionid) {
-        return axios.get(address+'/level/'+levelid+'/solutions/'+solutionid);
+async function getLevel(levelid) {
+    return axios.get(apiAddress + '/level/' + levelid);
+}
 
-    };
+async function getSolution(levelid, solutionid) {
+    return axios.get(apiAddress + '/level/' + levelid + '/solutions/' + solutionid);
 
-    export  { 
-        postRegister,
-        postLogin,
-        getChapter,
-        getSolution,
-        getLevel,
-        getLevelGoals,
-        getPossibleSoluctions,
-        postLevelSolution,
-        getLevels,
-        getLevelLeaderboard,
-        getLevelDocumentation,
-        getUserAchievements,
-        getUserDetails,
-        putUserName,
-        putUserPassword 
-    };
+}
 
-//tentei com e sem parenteses still nothi
+export {
+    postRegister,
+    postLogin,
+    getChapter,
+    getSolution,
+    getLevel,
+    getLevelGoals,
+    getPossibleSoluctions,
+    postLevelSolution,
+    getLevels,
+    getLevelLeaderboard,
+    getLevelDocumentation,
+    getUserAchievements,
+    getUserDetails,
+    putUserName,
+    putUserPassword
+};
