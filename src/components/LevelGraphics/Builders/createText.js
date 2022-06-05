0544c50744e447ef86e451786fb666de9b2950f4
DXOGO <diogophc@ua.pt>
2022-06-05 15:38:53 +0100

    adding all updated models and builder for all levels + adding level1_1

---
+++ src/components/LevelGraphics/Builders/createText.js
@@ -0,0 +1,71 @@
+import * as THREE from "three";
+
+import { resizeMovement } from '../Builders/tweenMotions';
+import { FontLoader } from 'three/examples/jsm/loaders/FontLoader';
+import { TextGeometry } from 'three/examples/jsm/geometries/TextGeometry';
+
+
+export const createText = (text, fontSize, textColor, hasSpeechBubble, hasTri, bubbleColor) => {
+    
+    const all = new THREE.Group();
+    
+    var fontloader = new FontLoader();
+    fontloader.load( '/fonts/roboto_regular.typeface.json', function ( font ) {
+
+        var textGeo = new TextGeometry( text, {
+            font: font,
+            size: fontSize,
+            height: 0,
+        } );
+
+
+    var final = new THREE.Mesh(textGeo, new THREE.MeshBasicMaterial( { color: textColor } ) );
+    
+    textGeo.computeBoundingBox();
+    const center = textGeo.boundingBox.getCenter(new THREE.Vector3());
+    const size = textGeo.boundingBox.getSize(new THREE.Vector3());
+    
+    final.position.set( -center.x, 6, 0.7 );
+    all.add(final);
+            
+    
+    if (hasSpeechBubble == true){
+
+        const box_color = new THREE.MeshBasicMaterial( {color: bubbleColor});
+        
+        // const p = new THREE.PlaneGeometry( size.x+1, 1.5 )
+        // p.rotateY( - Math.PI / 2 );
+
+        // speech bubble
+        const speech = new THREE.Mesh( new THREE.BoxGeometry( 1, 1.5, size.x+1 ), box_color);
+        speech.rotateY( - Math.PI / 2 );
+        speech.position.set( 0, 6.2, 0 ) // scale here
+        all.add(speech)
+    
+        if (hasTri == true){
+            const shape = new THREE.Shape();
+    
+            shape.moveTo( -0.6, 0 );
+            shape.lineTo( 0.6, 0 );
+            shape.lineTo( 0, 0.8 );
+            
+            const tri = new THREE.Mesh( new THREE.ShapeGeometry(shape), box_color);
+            tri.rotateZ(Math.PI);
+            tri.position.set( 0, 5.6, 0 ) // scale here
+            all.add(tri)
+        }
+    }
+
+    } );
+
+    all.position.set(0,0,0)
+    return all;   
+}
+
+export const showText = (text, scene, player) => {
+
+    text.scale.set(0, 0, 0)
+    text.position.set(player.position.x, player.position.y-2, player.position.z)
+    scene.add(text)
+    resizeMovement(text, 1, 1, 1, 1000, '+1000');
+}
---
+++ src/components/LevelGraphics/Chapter1_Introduction/Level1_1.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useRef } from "react";
+
+import * as THREE from "three";
+
+import {createScene, createCamera} from '../Builders/createEnvironment';
+import {resizeMovement} from '../Builders/tweenMotions';
+import {createPlayer} from '../Builders/createPlayer';
+import {createText} from '../Builders/createText';
+
+const TWEEN = require('@tweenjs/tween.js')
+
+let camera, scene, renderer;
+var step1 = true; var step2 = true;
+
+// * Hello World
+const Level1_1 = () => {
+ 
+    useEffect(() => {
+        // create camera and scene
+        // this is default camera 
+        //createCamera = (posx, posy, posz, lx, ly, lz) - pos (camera position), - l (camera lookAt)
+        camera = createCamera(0, 7, 24, 0, 0, 0);
+        scene = createScene();
+        
+        // * create player => THIS IS GOING TO BE TRIGGERED BY USER CODE - STEP 1
+        if (step1 === true) {
+            const player = createPlayer();
+            scene.add(player); 
+        }
+        
+        // ! this response comes from backend
+        const step2_response = "Hello World!";
+        
+        // * create text => THIS IS GOING TO BE TRIGGERED BY USER CODE -STEP 2
+        // const createText = (text, fontSize, textColor, hasSpeechBubble, hasTri, bubbleColor)
+        if (step2 === true){
+            const text =  createText(step2_response, 0.5, 0x171717, true, true, 0xffffff);
+            text.scale.set(0, 0, 0)
+            scene.add(text);
+            resizeMovement(text, 1, 1, 1, 1000, '+2000');
+        } 
+        
+        /////////////////////////////////////////////////////////////
+        renderer = new THREE.WebGLRenderer( { antialias: true } );
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth / 3, window.innerHeight - window.innerHeight / 5);
+        renderer.shadowMap.enabled = true;
+    
+        // const controls = new THREE.OrbitControls( camera, renderer.domElement );
+        document.getElementById("threejs").parentNode.replaceChild(renderer.domElement, document.getElementById("threejs"));
+
+        var onWindowResize = function () {
+            camera.aspect = window.innerWidth / 3 / (window.innerHeight - window.innerHeight / 5);
+            camera.updateProjectionMatrix();
+            renderer.setSize( window.innerWidth / 3, window.innerHeight - window.innerHeight / 5);
+
+        };
+
+        window.addEventListener( 'resize', onWindowResize );    
+
+        var animate = function(time) { 
+            TWEEN.update(time)
+            requestAnimationFrame(animate);
+            renderer.render( scene, camera ); 
+        };
+
+        animate();
+
+    }, []);
+
+    return(
+       <div id="threejs"></div>
+    );
+
+ };
+  
+ export default Level1_1;
