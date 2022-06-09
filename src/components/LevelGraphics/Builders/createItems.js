1e98161ed56dc0b3cf536fe50603de4a9dfd746a
DXOGO <diogophc@ua.pt>
2022-06-09 21:06:40 +0100

    updated builders

--- src/components/LevelGraphics/Builders/createItems.js
+++ src/components/LevelGraphics/Builders/createItems.js
@@ -13,31 +13,35 @@ import fencemodel from '../../../models/fence/fence.obj'
 import fencetexture from '../../../models/fence/fence.jpeg'
 
 
-export const createInventory = (scene) => {
-        // Instantiate a loader
-        const loader = new OBJLoader();
+export const createInventory = () => {
+    const group = new THREE.Group();
 
-        loader.load(
-            // resource URL
-            boxmodel,
-            // called when the resource is loaded
-            function ( inventory ) {
-                inventory.scale.set( 15, 15, 15 ) // scale here
-                inventory.position.set( 4.8, 2, 12 ) // scale here
-                inventory.rotateX(-Math.PI / 2);
+    // Instantiate a loader
+    const loader = new OBJLoader();
 
-                // For any meshes in the model, add our material.
-                inventory.traverse( function ( node ) {
-                    if ( node.isMesh ) node.material = new THREE.MeshPhongMaterial({color: 0x78522f});
-                } );
+    loader.load(
+        // resource URL
+        boxmodel,
+        // called when the resource is loaded
+        function ( inventory ) {
+            inventory.scale.set( 15, 15, 15 ) // scale here
+            inventory.position.set( 4.8, 2, 12 ) // scale here
+            inventory.rotateX(-Math.PI / 2);
 
-                scene.add(inventory)
-            }, 
-            // called while loading is progressing
-            function ( xhr ) { console.log( ('inventory ' + xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
-            // called when loading has errors
-            function ( error ) { console.log( 'An error happened '); }
-        );
+            // For any meshes in the model, add our material.
+            inventory.traverse( function ( node ) {
+                if ( node.isMesh ) node.material = new THREE.MeshPhongMaterial({color: 0x78522f});
+            } );
+
+            group.add(inventory)
+
+        }, 
+        // called while loading is progressing
+        function ( xhr ) { console.log( ('inventory ' + xhr.loaded / xhr.total * 100 ) + '% loaded' ); },
+        // called when loading has errors
+        function ( error ) { console.log( 'An error happened '); }
+    );
+    return group;    
 }
 
 export const createSword = () => {
